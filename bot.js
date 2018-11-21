545732a9307ee1d1860529c3777e5e432cd242d2
eyadasd <37232558+eyadasd@users.noreply.github.com>
2018-11-21 12:01:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "513709288347140097"; // ايدي السررفر
-var channel = "513709288825552907";//ايدي الروم
+var server = "514741138398380032"; // ايدي السررفر
+var channel = "514741138398380034";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
